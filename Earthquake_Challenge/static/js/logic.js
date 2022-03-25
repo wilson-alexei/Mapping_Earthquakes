b1a59d786ea27ad50ff905af017f7f38bb36ba1e
Wilson Alexei <wilsonalexei123@yahoo.com>
2022-03-25 16:45:13 -0400

    Add files via upload

---
+++ Earthquake_Challenge/static/js/logic.js
@@ -0,0 +1,54 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "satelliteStreets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+//streets.addTo(map);
+
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/wilson-alexei/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
+
+
